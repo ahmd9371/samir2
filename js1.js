ab860ba7085c49de0eb10821464b7899a1f911fb
ahmd9371 <117758038+ahmd9371@users.noreply.github.com>
2022-12-16 10:46:24 +0200

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,18 @@
+let tak = document.getElementById('img');
+let backgrnd = document.querySelector('.continerrr');
+let txt = document.getElementById('text1')
+function phones(photo) {
+    tak.src = photo;
+}
+
+function colors(color) {
+    backgrnd.style.background = color;
+
+}
+
+function texts(x) {
+    txt.style.color = x;
+}
+
+
+
